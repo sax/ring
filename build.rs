d6cbc085513c0b7786ca97bd9609d1770e8d3dae
Eric Saxby <sax@livinginthepast.org>
2024-05-06 14:18:46 -0700

    Add support for visionOS and watchOS

--- build.rs
+++ build.rs
@@ -255,7 +255,7 @@ const NASM: &str = "nasm";
 
 /// Operating systems that have the same ABI as macOS on every architecture
 /// mentioned in `ASM_TARGETS`.
-const MACOS_ABI: &[&str] = &["ios", MACOS, "tvos"];
+const MACOS_ABI: &[&str] = &["ios", MACOS, "tvos", "visionos", "watchos"];
 
 const MACOS: &str = "macos";
 const WINDOWS: &str = "windows";
--- src/rand.rs
+++ src/rand.rs
@@ -138,7 +138,9 @@ impl crate::sealed::Sealed for SystemRandom {}
     target_os = "redox",
     target_os = "solaris",
     target_os = "tvos",
+    target_os = "visionos",
     target_os = "vita",
+    target_os = "watchos",
     target_os = "windows",
     all(
         target_arch = "wasm32",
